7e76d48b2397a4312f251eaa6320a145dd2e2ce8
ahmad glitch <ahmadhamid658@yahoo.com>
2024-12-26 23:25:19 +0500

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Projects from "./pages/Projects";
 
 export default function App() {
   return (
-    <div>
-      app
-    </div>
+
+    <Router>
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </Router>
   )
 }
