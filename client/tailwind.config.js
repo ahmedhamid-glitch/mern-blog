4b9cc71213d629b6ff5a34b2feb2aae90090e9bf
ahmad glitch <ahmadhamid658@yahoo.com>
2024-12-27 17:53:53 +0500

    create the header section

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,32 @@
+// /** @type {import('tailwindcss').Config} */
+// import flowbite from 'flowbite/plugin';
+
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//     "node_modules/flowbite-react/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     flowbite,
+//   ],
+// };
+
+import flowbite from "flowbite-react/tailwind";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  content: [
+    // ...
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
+  ],
+  plugins: [
+    // ...
+    flowbite.plugin(),
+  ],
 };
